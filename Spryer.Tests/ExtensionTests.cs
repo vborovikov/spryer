0c6c36aab1bec424d0c0fb80fea5f66cb8f1c6dd
Vladislav Borovikov <vborovikov@live.ru>
2024-11-15 05:10:52 +0500

    added throwing an exception on string max length exceeded

---
+++ Spryer.Tests/ExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Spryer.Tests;
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ExtensionTests
+{
+    [TestMethod]
+    public void AsNVarChar_StringTooLong_Throws()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => "String too long".AsNVarChar(3, throwOnMaxLength: true));
+    }
+
+    [TestMethod]
+    public void AsNVarChar_StringTooLong_Truncated()
+    {
+        var dbString = "String too long".AsNVarChar(3);
+
+        Assert.AreEqual(3, dbString.Value?.Length);
+    }
+}
--- Spryer/DapperExtensions.cs
+++ Spryer/DapperExtensions.cs
@@ -6,38 +6,46 @@ using Dapper;
 /// A static class containing extension methods for Dapper.
 /// </summary>
 public static class DapperExtensions
-{   
+{
     /// <summary>
     /// Converts a string value to a <see cref="DbString"/> instance with a fixed length, using the ANSI encoding.
     /// </summary>
     /// <param name="str">The string value to convert.</param>
     /// <param name="maxLength">The maximum length of the resulting <see cref="DbString"/> instance.</param>
+    /// <param name="throwOnMaxLength">Whether to throw an exception if the maximum length is exceeded.</param>
     /// <returns>A <see cref="DbString"/> instance with a fixed length, using the ANSI encoding.</returns>
-    public static DbString AsChar(this string? str, int maxLength = -1) => str.AsDbString(maxLength, isFixedLength: true, isAnsi: true);
+    public static DbString AsChar(this string? str, int maxLength = -1, bool throwOnMaxLength = false) =>
+        str.AsDbString(maxLength, isFixedLength: true, isAnsi: true, throwOnMaxLength);
 
     /// <summary>
     /// Converts a string value to a <see cref="DbString"/> instance with a variable length, using the ANSI encoding.
     /// </summary>
     /// <param name="str">The string value to convert.</param>
     /// <param name="maxLength">The maximum length of the resulting <see cref="DbString"/> instance.</param>
+    /// <param name="throwOnMaxLength">Whether to throw an exception if the maximum length is exceeded.</param>
     /// <returns>A <see cref="DbString"/> instance with a variable length, using the ANSI encoding.</returns>
-    public static DbString AsVarChar(this string? str, int maxLength = -1) => str.AsDbString(maxLength, isFixedLength: false, isAnsi: true);
+    public static DbString AsVarChar(this string? str, int maxLength = -1, bool throwOnMaxLength = false) =>
+        str.AsDbString(maxLength, isFixedLength: false, isAnsi: true, throwOnMaxLength);
 
     /// <summary>
     /// Converts a string value to a <see cref="DbString"/> instance with a fixed length, using the Unicode encoding.
     /// </summary>
     /// <param name="str">The string value to convert.</param>
     /// <param name="maxLength">The maximum length of the resulting <see cref="DbString"/> instance.</param>
+    /// <param name="throwOnMaxLength">Whether to throw an exception if the maximum length is exceeded.</param>
     /// <returns>A <see cref="DbString"/> instance with a fixed length, using the Unicode encoding.</returns>
-    public static DbString AsNChar(this string? str, int maxLength = -1) => str.AsDbString(maxLength, isFixedLength: true, isAnsi: false);
+    public static DbString AsNChar(this string? str, int maxLength = -1, bool throwOnMaxLength = false) =>
+        str.AsDbString(maxLength, isFixedLength: true, isAnsi: false, throwOnMaxLength);
 
     /// <summary>
     /// Converts a string value to a <see cref="DbString"/> instance with a variable length, using the Unicode encoding.
     /// </summary>
     /// <param name="str">The string value to convert.</param>
     /// <param name="maxLength">The maximum length of the resulting <see cref="DbString"/> instance.</param>
+    /// <param name="throwOnMaxLength">Whether to throw an exception if the maximum length is exceeded.</param>
     /// <returns>A <see cref="DbString"/> instance with a variable length, using the Unicode encoding.</returns>
-    public static DbString AsNVarChar(this string? str, int maxLength = -1) => str.AsDbString(maxLength, isFixedLength: false, isAnsi: false);
+    public static DbString AsNVarChar(this string? str, int maxLength = -1, bool throwOnMaxLength = false) =>
+        str.AsDbString(maxLength, isFixedLength: false, isAnsi: false, throwOnMaxLength);
 
     /// <summary>
     /// Converts a string value to a <see cref="DbString"/> instance with the specified properties.
@@ -46,14 +54,19 @@ public static class DapperExtensions
     /// <param name="maxLength">The maximum length of the resulting <see cref="DbString"/> instance.</param>
     /// <param name="isFixedLength">Whether the resulting <see cref="DbString"/> instance is fixed length.</param>
     /// <param name="isAnsi">Whether the resulting <see cref="DbString"/> instance uses ANSI encoding.</param>
+    /// <param name="throwOnMaxLength">Whether to throw an exception if the maximum length is exceeded.</param>
     /// <returns>A <see cref="DbString"/> instance with the specified properties.</returns>
-    public static DbString AsDbString(this string? str, int maxLength = -1, bool isFixedLength = false, bool isAnsi = false) => new DbString
-    {
-        Value = maxLength > 0 && str?.Length > maxLength ? str[..maxLength] : str,
-        IsFixedLength = maxLength > 0 && isFixedLength,
-        Length = maxLength < 0 ? -1 : maxLength,
-        IsAnsi = isAnsi,
-    };
+    public static DbString AsDbString(this string? str, int maxLength = -1,
+        bool isFixedLength = false, bool isAnsi = false, bool throwOnMaxLength = false) => 
+        new()
+        {
+            Value = maxLength > 0 && str?.Length > maxLength ?
+                throwOnMaxLength ? throw new ArgumentOutOfRangeException(nameof(str)) : str[..maxLength] :
+                str,
+            IsFixedLength = maxLength > 0 && isFixedLength,
+            Length = maxLength < 0 ? -1 : maxLength,
+            IsAnsi = isAnsi,
+        };
 
     /// <summary>
     /// Converts an enumeration value to a <see cref="DbEnum{TEnum}"/> instance.
