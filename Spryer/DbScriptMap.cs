5f3c8a153fcf5b94e0bd3d1d355603bd2f9c3cbc
Vladislav Borovikov <vborovikov@live.ru>
2025-02-15 15:50:24 +0500

    refactored Loader

--- Spryer/DbScriptMap.cs
+++ Spryer/DbScriptMap.cs
@@ -4,17 +4,18 @@ using System;
 using System.Collections.Frozen;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
-using ScriptMap = System.Collections.Frozen.FrozenDictionary<string, string>;
+using ScriptDictionary = System.Collections.Frozen.FrozenDictionary<string, string>;
 
 /// <summary>
 /// Represents a collection of SQL scripts loaded from an external source.
 /// </summary>
 [DebuggerDisplay("{Source}/{Version}: {Count}")]
-public class DbScriptMap
+public sealed class DbScriptMap
 {
     private const string ScriptsFileName = "Scripts";
     private const string ScriptsFileExt = ".sql";
@@ -22,13 +23,13 @@ public class DbScriptMap
     /// <summary>
     /// An empty instance of <see cref="DbScriptMap"/>.
     /// </summary>
-    public static readonly DbScriptMap Empty = new(string.Empty, new(), ScriptMap.Empty);
+    public static readonly DbScriptMap Empty = new(string.Empty, new(), ScriptDictionary.Empty);
 
     private readonly string source;
     private readonly Version version;
-    private readonly ScriptMap scripts;
+    private readonly ScriptDictionary scripts;
 
-    private DbScriptMap(string source, Version version, ScriptMap scripts)
+    private DbScriptMap(string source, Version version, ScriptDictionary scripts)
     {
         this.source = source;
         this.version = version;
@@ -67,146 +68,185 @@ public class DbScriptMap
     /// <returns>A collection of SQL scripts.</returns>
     public static DbScriptMap Load(string? fileName = null)
     {
-        if (TryLoad(fileName, assembly: default, out var scriptMap))
-            return scriptMap;
+        var loader = new Loader { FileName = fileName };
 
-        if (TryLoad(fileName, Assembly.GetEntryAssembly(), out scriptMap))
-            return scriptMap;
+        if (loader.TryLoadScripts())
+            return loader.ScriptMap;
 
-        if (TryLoad(fileName, Assembly.GetCallingAssembly(), out scriptMap))
-            return scriptMap;
+        loader.Assembly = Assembly.GetEntryAssembly();
+        if (loader.TryLoadScripts())
+            return loader.ScriptMap;
+
+        loader.Assembly = Assembly.GetCallingAssembly();
+        if (loader.TryLoadScripts())
+            return loader.ScriptMap;
 
         return Empty;
     }
 
-    private static bool TryLoad(string? fileName, Assembly? assembly, out DbScriptMap scriptMap)
+    /// <summary>
+    /// Represents a loader fro the script collection.
+    /// </summary>
+    public class Loader
     {
-        foreach (var scriptFileName in EnumerateFileNames(assembly, fileName))
+        /// <summary>
+        /// Gets or sets the desired file name fro the script collection.
+        /// </summary>
+        public string? FileName { get; init; }
+
+        /// <summary>
+        /// Gets or sets the assembly to look for the script collection.
+        /// </summary>
+        public Assembly? Assembly { get; set; }
+
+        /// <summary>
+        /// Gets the loaded script collection.
+        /// </summary>
+        public DbScriptMap? ScriptMap { get; private set; }
+
+        /// <summary>
+        /// Tries to load the script collection.
+        /// </summary>
+        /// <returns><c>true</c> if the script collection is successfully loaded, <c>false</c> otherwise.</returns>
+        [MemberNotNullWhen(true, nameof(ScriptMap))]
+        public bool TryLoadScripts()
         {
-            if (string.IsNullOrWhiteSpace(scriptFileName))
-                continue;
-
-            // scripts are loaded from the external files or the assembly resources
-
-            foreach (var scriptFilePath in EnumerateFilePaths(assembly, scriptFileName))
+            foreach (var scriptFileName in EnumerateFileNames())
             {
-                if (string.IsNullOrWhiteSpace(scriptFilePath))
+                if (string.IsNullOrWhiteSpace(scriptFileName))
                     continue;
 
+                // scripts are loaded from the external files or the assembly resources
 
-                Debug.WriteLine($"Spryer: looking for '{scriptFilePath}'");
+                foreach (var scriptFilePath in EnumerateFilePaths(scriptFileName))
+                {
+                    if (string.IsNullOrWhiteSpace(scriptFilePath))
+                        continue;
 
-                if (File.Exists(scriptFilePath) && TryLoad(scriptFilePath, File.OpenRead(scriptFilePath), out scriptMap))
+
+                    Debug.WriteLine($"Spryer: looking for '{scriptFilePath}'");
+
+                    if (File.Exists(scriptFilePath) && TryLoadStream(scriptFilePath, File.OpenRead(scriptFilePath)))
+                    {
+                        return true;
+                    }
+                }
+
+                if (this.Assembly is not null &&
+                    Array.Find(this.Assembly.GetManifestResourceNames(), n => n.EndsWith(scriptFileName, StringComparison.OrdinalIgnoreCase)) is string resourceName &&
+                    this.Assembly.GetManifestResourceStream(resourceName) is Stream resourceStream && TryLoadStream(resourceName, resourceStream))
                 {
                     return true;
                 }
             }
 
-            if (assembly is not null &&
-                Array.Find(assembly.GetManifestResourceNames(), n => n.EndsWith(scriptFileName, StringComparison.OrdinalIgnoreCase)) is string resourceName &&
-                assembly.GetManifestResourceStream(resourceName) is Stream resourceStream && TryLoad(resourceName, resourceStream, out scriptMap))
+            return false;
+        }
+
+        [MemberNotNullWhen(true, nameof(ScriptMap))]
+        private bool TryLoadStream(string scriptSource, Stream scriptStream)
+        {
+            var scriptText = GetScriptText(scriptStream);
+
+            if (!string.IsNullOrWhiteSpace(scriptText))
             {
+                var scripts = ParseScripts(scriptText, out var version);
+                this.ScriptMap = new(scriptSource, version, scripts);
+
+                Debug.WriteLine($"Spryer: {scripts.Count} scripts found in '{scriptSource}'");
                 return true;
             }
+
+            return false;
         }
 
-        scriptMap = Empty;
-        return false;
-    }
-
-    private static bool TryLoad(string scriptSource, Stream scriptStream, out DbScriptMap scriptMap)
-    {
-        using var reader = new StreamReader(scriptStream, Encoding.UTF8, detectEncodingFromByteOrderMarks: true, leaveOpen: false);
-        var scriptText = reader.ReadToEnd();
-
-        if (!string.IsNullOrWhiteSpace(scriptText))
+        /// <summary>
+        /// Reads the script text from the given stream.
+        /// </summary>
+        /// <param name="scriptStream">The script stream.</param>
+        /// <returns>A script raw text.</returns>
+        protected virtual string GetScriptText(Stream scriptStream)
         {
-            var scripts = ParseScripts(scriptText, out var version);
-            scriptMap = new(scriptSource, version, scripts);
-
-            Debug.WriteLine($"Spryer: {scripts.Count} scripts found in '{scriptSource}'");
-            return true;
+            using var reader = new StreamReader(scriptStream, Encoding.UTF8, detectEncodingFromByteOrderMarks: true, leaveOpen: false);
+            return reader.ReadToEnd();
         }
 
-        scriptMap = Empty;
-        return false;
-    }
-
-    private static IEnumerable<string?> EnumerateFilePaths(Assembly? assembly, string fileName)
-    {
-        if (assembly is not null)
+        private IEnumerable<string?> EnumerateFilePaths(string scriptFileName)
         {
-            if (Path.GetDirectoryName(assembly.Location) is { Length: > 0 } assemblyDir)
-                yield return Path.Combine(assemblyDir, fileName);
-
-            yield return Path.Combine(
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), assembly.GetName().Name!),
-                fileName);
-        }
-        else
-        {
-            if (Path.GetDirectoryName(Environment.ProcessPath) is { Length: > 0 } processDir)
-                yield return Path.Combine(processDir, fileName);
-
-            if (Path.GetFileNameWithoutExtension(Environment.ProcessPath) is { Length: > 0 } processName)
+            if (this.Assembly is not null)
             {
+                if (Path.GetDirectoryName(this.Assembly.Location) is { Length: > 0 } assemblyDir)
+                    yield return Path.Combine(assemblyDir, scriptFileName);
+
                 yield return Path.Combine(
-                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), processName),
-                    fileName);
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), this.Assembly.GetName().Name!),
+                    scriptFileName);
             }
-        }
-    }
-
-    private static IEnumerable<string?> EnumerateFileNames(Assembly? assembly, string? fileName)
-    {
-        if (!string.IsNullOrWhiteSpace(fileName))
-            yield return fileName;
-
-        yield return Path.ChangeExtension(Path.GetFileName(Environment.ProcessPath), ScriptsFileExt);
-        yield return Path.GetFileNameWithoutExtension(Environment.ProcessPath) + "." + ScriptsFileName + ScriptsFileExt;
-
-        if (assembly is not null)
-        {
-            yield return assembly.GetName().Name + ScriptsFileExt;
-            yield return assembly.GetName().Name + "." + ScriptsFileName + ScriptsFileExt;
-            yield return Path.ChangeExtension(Path.GetFileName(assembly.Location), ScriptsFileExt);
-        }
-
-        yield return ScriptsFileName + ScriptsFileExt;
-    }
-
-    private static ScriptMap ParseScripts(ReadOnlySpan<char> text, out Version version)
-    {
-        // any sql statements before the first pragma are ignored
-        var start = text.IndexOf(Pragma.Prefix, StringComparison.Ordinal);
-        if (start < 0)
-        {
-            version = Empty.Version;
-            return ScriptMap.Empty;
-        }
-
-        text = text[start..];
-
-        var foundVersion = default(Version);
-        var scripts = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-        foreach (var pragma in EnumeratePragmas(text))
-        {
-            if (pragma.Name.Equals(Pragma.Script, StringComparison.OrdinalIgnoreCase))
+            else
             {
-                ref var script = ref CollectionsMarshal.GetValueRefOrAddDefault(scripts, pragma.Meta.ToString(), out _);
-                script = pragma.Data.ToString();
-            }
-            else if (pragma.Name.Equals(Pragma.Version, StringComparison.OrdinalIgnoreCase))
-            {
-                Version.TryParse(pragma.Meta, out foundVersion);
+                if (Path.GetDirectoryName(Environment.ProcessPath) is { Length: > 0 } processDir)
+                    yield return Path.Combine(processDir, scriptFileName);
+
+                if (Path.GetFileNameWithoutExtension(Environment.ProcessPath) is { Length: > 0 } processName)
+                {
+                    yield return Path.Combine(
+                        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), processName),
+                        scriptFileName);
+                }
             }
         }
 
-        version = foundVersion ?? Empty.Version;
-        return scripts.ToFrozenDictionary(StringComparer.OrdinalIgnoreCase);
-    }
+        private IEnumerable<string?> EnumerateFileNames()
+        {
+            if (!string.IsNullOrWhiteSpace(this.FileName))
+                yield return this.FileName;
 
-    private static PragmaEnumerator EnumeratePragmas(ReadOnlySpan<char> text) => new(text);
+            yield return Path.ChangeExtension(Path.GetFileName(Environment.ProcessPath), ScriptsFileExt);
+            yield return Path.GetFileNameWithoutExtension(Environment.ProcessPath) + "." + ScriptsFileName + ScriptsFileExt;
+
+            if (this.Assembly is not null)
+            {
+                yield return this.Assembly.GetName().Name + ScriptsFileExt;
+                yield return this.Assembly.GetName().Name + "." + ScriptsFileName + ScriptsFileExt;
+                yield return Path.ChangeExtension(Path.GetFileName(this.Assembly.Location), ScriptsFileExt);
+            }
+
+            yield return ScriptsFileName + ScriptsFileExt;
+        }
+
+        private static ScriptDictionary ParseScripts(ReadOnlySpan<char> text, out Version version)
+        {
+            // any sql statements before the first pragma are ignored
+            var start = text.IndexOf(Pragma.Prefix, StringComparison.Ordinal);
+            if (start < 0)
+            {
+                version = Empty.Version;
+                return ScriptDictionary.Empty;
+            }
+
+            text = text[start..];
+
+            var foundVersion = default(Version);
+            var scripts = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            foreach (var pragma in EnumeratePragmas(text))
+            {
+                if (pragma.Name.Equals(Pragma.Script, StringComparison.OrdinalIgnoreCase))
+                {
+                    ref var script = ref CollectionsMarshal.GetValueRefOrAddDefault(scripts, pragma.Meta.ToString(), out _);
+                    script = pragma.Data.ToString();
+                }
+                else if (pragma.Name.Equals(Pragma.Version, StringComparison.OrdinalIgnoreCase))
+                {
+                    Version.TryParse(pragma.Meta, out foundVersion);
+                }
+            }
+
+            version = foundVersion ?? Empty.Version;
+            return scripts.ToFrozenDictionary(StringComparer.OrdinalIgnoreCase);
+        }
+
+        private static PragmaEnumerator EnumeratePragmas(ReadOnlySpan<char> text) => new(text);
+    }
 
     [DebuggerDisplay("@{Name,nq} {Meta,nq}")]
     private readonly ref struct Pragma
