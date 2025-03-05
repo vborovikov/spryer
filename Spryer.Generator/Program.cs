60155287c379fdb79ba9c2ce912d3bad61f808f4
Vladislav Borovikov <vborovikov@live.ru>
2025-03-05 15:38:58 +0500

    created code generator project

---
+++ Spryer.Generator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Spryer.Generator;
+
+static class Program
+{
+    static int Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.Error.WriteLine("Usage: SpryerGen <script_file_name>");
+            return 1;
+        }
+
+        var scriptFileName = Path.GetFullPath(args[0]);
+        var scriptMapLoader = new DbScriptMap.Loader
+        {
+            FileName = scriptFileName
+        };
+        if (!scriptMapLoader.TryLoadScripts())
+        {
+            Console.Error.WriteLine($"No scripts found in '{scriptFileName}'");
+            return 2;
+        }
+
+        var scriptMap = scriptMapLoader.GetScriptMap();
+        Console.Error.WriteLine($"{scriptMap.Count} scripts loaded from '{scriptMap.Source}'");
+
+        return 0;
+    }
+}
